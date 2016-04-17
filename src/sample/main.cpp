ac73ec4b392068add4a0fb39fe846a392283c9fd
wugh7125 <wray00@yeah.net>
2016-04-17 15:52:53 +0800

    Change file's properties

--- src/sample/main.cpp
+++ src/sample/main.cpp
@@ -1,21 +1,16 @@
 #include <iostream>
-#include <string>
-
-#include "base/strings/utf_string_conversions.h"
 #include "base/strings/sys_string_conversions.h"
-#include "base/file_util.h"
-
+#include "base/strings/utf_string_conversions.h"
 
 int main() {
-  std::string a = "测试abc中文";
+  std::string a = "娴嬭瘯";
   std::wstring w = base::SysNativeMBToWide(a);
   std::string u = base::WideToUTF8(w);
   std::wstring ww = base::UTF8ToWide(u);
-
-  base::FilePath path(L"D:\\software\\Wireshark-win64-1.8.4中文版");
-  base::FilePath dir = path.DirName();
-  base::FilePath base = path.BaseName();
-
-  system("pause");
+  
+  std::cout << a << std::endl;
+  
+  std::cout << "Hello World" << std::endl;
   return 0;
 }
+
